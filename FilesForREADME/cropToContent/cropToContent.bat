fc95267187b6294bd24f95128c4178e69d0bb322
Michael Sohnen <michaelsohnenacademic@gmail.com>
2022-04-20 18:42:45 -0500

    Stared work on a new script for image processing.

---
+++ FilesForREADME/cropToContent/cropToContent.bat
@@ -0,0 +1,1 @@
+py cropToContent.py
---
+++ FilesForREADME/cropToContent/cropToContent.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import numpy as np
+import os
+
+#credit: creating a file picker dialog: https://stackoverflow.com/a/3579783/5166365
+from tkinter.filedialog import askopenfilename
+filename = askopenfilename()
+
+xmin = float("inf")
+xmax = float("-inf")
+ymin = float("inf")
+ymax = float("-inf")
+
+img = Image.open(filename)
+
+sz1=img.size
+
+for x,y in np.ndindex(img.size):
+    data=img.getpixel((x,y))
+    if data != (255,255,255,255):
+        xmin=min(x,xmin)
+        ymin=min(y,ymin)
+        xmax=max(x,xmax)
+        ymax=max(y,ymax)
+
+img=img.crop((xmin,ymin,xmax,ymax))
+
+dirn = os.path.dirname(filename)
+basen = os.path.basename(filename)
+imagen = ".".join(basen.split(".")[:-1])
+extn = basen.split(".")[-1]
+
+sz2=img.size
+
+print(sz1,sz2)
+
+#%SystemRoot%\System32\rundll32.exe "%ProgramFiles%\Windows Photo
+#Viewer\PhotoViewer.dll", ImageView_Fullscreen %1
+
+outfn = os.path.join(dirn,imagen+"_cropped."+extn).replace("/","\\")
+
+
+img.save(outfn)
+os.system("%SystemRoot%\\explorer.exe \"{}\"".format(outfn))
+
