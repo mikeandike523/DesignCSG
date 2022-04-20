6cc437764eb7a6f4b4e1b8dd39b53621cb2db335
Michael Sohnen <michaelsohnenacademic@gmail.com>
2022-04-19 21:33:45 -0500

    Deleted unnecessary files.

--- show_box_points.py
+++
@@ -1,8 +0,0 @@
-import pptk
-import numpy as np
-with open("box_points.txt") as fl:
-    items = []
-    for line in fl:
-        items.append(list(map(float,line.split(" "))))
-
-pptk.viewer(np.array(items))
