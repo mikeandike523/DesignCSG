18d84e2fb4e7d159e50454da88333f1066783f52
Michael Sohnen <michaelsohnenacademic@gmail.com>
2022-04-20 19:02:28 -0500

    Further work on readme.
    Former-commit-id: be399b58d1479897f89cf4d877050f43565b12f5

--- FilesForREADME/matchSizes/matchSizes.py
+++ FilesForREADME/matchSizes/matchSizes.py
@@ -4,42 +4,48 @@ import os
 
 #credit: creating a file picker dialog: https://stackoverflow.com/a/3579783/5166365
 from tkinter.filedialog import askopenfilename
-filename = askopenfilename()
+filepath1 = askopenfilename()
+filepath2 = askopenfilename()
 
-xmin = float("inf")
-xmax = float("-inf")
-ymin = float("inf")
-ymax = float("-inf")
-
-img = Image.open(filename)
-
-sz1=img.size
-
-for x,y in np.ndindex(img.size):
-    data=img.getpixel((x,y))
-    if data != (255,255,255,255):
-        xmin=min(x,xmin)
-        ymin=min(y,ymin)
-        xmax=max(x,xmax)
-        ymax=max(y,ymax)
-
-img=img.crop((xmin,ymin,xmax,ymax))
-
-dirn = os.path.dirname(filename)
-basen = os.path.basename(filename)
-imagen = ".".join(basen.split(".")[:-1])
-extn = basen.split(".")[-1]
-
-sz2=img.size
-
-print(sz1,sz2)
-
-#%SystemRoot%\System32\rundll32.exe "%ProgramFiles%\Windows Photo
-#Viewer\PhotoViewer.dll", ImageView_Fullscreen %1
-
-outfn = os.path.join(dirn,imagen+"_cropped."+extn).replace("/","\\")
+background = (255,255,255)
 
 
-img.save(outfn)
-os.system("%SystemRoot%\\explorer.exe \"{}\"".format(outfn))
+img1 = Image.open(filepath1)
+img2 = Image.open(filepath2)
 
+sz1 = img1.size
+sz2 = img2.size
+
+maxWidth = max(sz1[0],sz2[0])
+maxHeight = max(sz1[1],sz2[1])
+
+padW1 = maxWidth - sz1[0]
+padH1 = maxHeight - sz1[1]
+
+padW2 = maxWidth - sz2[0]
+padH2 = maxHeight -sz2[1]
+
+padW1Left = padW1//2; padW1Right = padW1-padW1//2
+padH1Left = padH1//2; padH1Right = padH1-padH1//2
+padW2Left = padW2//2; padW2Right = padW2-padW2//2
+padH2Left = padH2//2; padH2Right = padH2-padH2//2
+
+newimg1 = Image.new(size=(maxWidth,maxHeight),color = background,mode="RGB")
+newimg2 = Image.new(size=(maxWidth,maxHeight),color = background,mode="RGB")
+
+newimg1.paste(img1,(padW1Left,padW1Right))
+newimg2.paste(img2,(padW2Left,padW2Right))
+
+
+
+dirn1 = os.path.dirname(filepath1)
+dirn2 = os.path.dirname(filepath2)
+ext1 = os.path.basename(filepath1).split(".")[-1]
+ext2 = os.path.basename(filepath1).split(".")[-1]
+n1 = ".".join(os.path.basename(filepath1).split(".")[0:-1])
+n2 = ".".join(os.path.basename(filepath2).split(".")[0:-1])
+
+newimg1.save(os.path.join(dirn1,n1+"_padded"+"."+ext1))
+newimg2.save(os.path.join(dirn2,n2+"_padded"+"."+ext2))
+
+print(n1,n2)
