5ea055374b74459b576036946ea6966086e211ff
Michael Sohnen <michaelsohnenacademic@gmail.com>
2022-04-21 23:21:27 -0500

    Progress.
    Former-commit-id: 7500f1c44837900da8751fb8cb9d1c6cfd4709ff

--- master/compiled.py
+++ master/compiled.py
@@ -1,6 +1,20 @@
 from DesignCSG import *
 from designlibrary import *
 
+define_auxillary_function("""
+#define Vector3f(x,y,z) (float3)((float)(x),(float)(y),(float)(z))
+float box(float3 v){
+	
+	return T_max(fabs(v.x)-0.5,T_max(fabs(v.y)-0.5,fabs(v.z)-0.5));
+
+}
+
+""")
+
+scene_brush = define_brush(body="""
+	return box(Vector3f(v.x,v.y-getAD(AD_DT2,1),v.z));
+""")
+
 def vec3(x,y,z):
 	return np.array([x,y,z])
 
@@ -16,9 +30,7 @@ cmap = font.getBestCmap()
 charC_mapped = cmap[ord('C')]
 print(charC_mapped)
 
-box_brush = define_brush(body="return T_max(fabs(v.x)-0.5,T_max(fabs(v.y)-0.5,fabs(v.z)-0.5));")
-
-draw(box_brush,Transform.initial(
+draw(scene_brush,Transform.initial(
 	position=vec3(0.0,0.0,0.0),
 	yaw=0,
 	pitch=0,
@@ -26,6 +38,6 @@ draw(box_brush,Transform.initial(
 	scale=vec3(1.0,1.0,1.0)
 ))
 
-compiler.addArbitraryData("cCurves",[1,2,3,4])
-
+addArbitraryData("DT1",[1,2,3,4])
+addArbitraryData("DT2",[1,-2,4,3])
 commit()
--- master/k1build.cl
+++ master/k1build.cl
@@ -26,7 +26,7 @@
 #define T_min(a,b) (a<b?a:b)
 #define T_max(a,b) (a>b?a:b)
 
-#define getAD(name,offset) (arbitrary_Data[name+offset])
+#define getAD(name,offset) (arbitrary_data[name+offset])
 
 
 float sdf_bank(double3 v, unsigned char shape_id);
@@ -575,12 +575,22 @@ __kernel void  k1(
  
 }
         
-        #define AD_CCURVES 0
+        #define AD_DT1 0
+#define AD_DT2 4
 
 
         
 
          
+
+#define Vector3f(x,y,z) (float3)((float)(x),(float)(y),(float)(z))
+float box(float3 v){
+	
+	return T_max(fabs(v.x)-0.5,T_max(fabs(v.y)-0.5,fabs(v.z)-0.5));
+
+}
+
+
 
         
         float sd0( double3 v){
@@ -623,7 +633,9 @@ __kernel void  k1(
 
         float sd4( double3 v){
 
-            return T_max(fabs(v.x)-0.5,T_max(fabs(v.y)-0.5,fabs(v.z)-0.5));
+            
+	return box(Vector3f(v.x,v.y-getAD(AD_DT2,1),v.z));
+
 
         }
         
