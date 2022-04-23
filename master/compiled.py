a13f967c35b343c7e08d7228718379c9a7b3ef02
Michael Sohnen <michaelsohnenacademic@gmail.com>
2022-04-23 00:05:11 -0500

    Progress.

--- master/compiled.py
+++ master/compiled.py
@@ -3,6 +3,184 @@ from designlibrary import *
 from fontTools.pens.ttGlyphPen import TTGlyphPen
 from fontTools.pens.pointInsidePen import PointInsidePen
 
+# ######
+#Courtesy of everestial007 on StackOverflow
+#https://stackoverflow.com/a/42815781/5166365
+from fontTools.ttLib import TTFont
+font = TTFont('Designs/Roboto-Black.ttf')
+# ######
+
+cmap = font.getBestCmap()
+glyphSet = font.getGlyphSet()
+
+
+AXES_XYZ  = -1
+AXES_XY = 0
+AXES_YZ = 1
+AXES_ZX = 2
+
+def vec3(x,y,z):
+	return np.array([x,y,z])
+
+def point(a,b):
+	return (a,b)
+
+def midpoint(A,B):
+	return 0.5*(A+B)
+
+class Curve:
+
+	def __init__(self,A,B,C,thickness = 0.075,axesTag=AXES_XY):
+		self.A = A
+		self.B = B
+		self.C = C
+
+		self.thickness = thickness
+		self.axesTag =axesTag
+
+def getFontData(chr):
+	pen = TTGlyphPen(glyphSet)
+	g = glyphSet[cmap[ord(chr)]]
+	g.draw(pen)
+	g=pen.glyph()
+	return g.getCoordinates(glyphSet._glyphs)
+
+def getScalers(letter):
+	pts = getFontData(letter)[0]
+	minX = -1
+	maxX = 1
+	minY = -1
+	maxY = 1
+	xvals = []
+	yvals = []
+	for pt in pts:
+		xvals.append(pt[0])
+		yvals.append(pt[1])
+	_minX = np.min(xvals)
+	_maxX = np.max(xvals)
+	_minY = np.min(yvals)
+	_maxY = np.max(yvals)
+	rescaleX = lambda x: minX + (maxX-minX) * (x-_minX) / (_maxX-_minX)
+	rescaleY = lambda y: minY + (maxY-minY)* (y-_minY) / (_maxY-_minY)
+	return rescaleX, rescaleY
+
+def getInverseScalers(letter):
+	pts = getFontData(letter)[0]
+	_minX = -1
+	_maxX = 1
+	_minY = -1
+	_maxY = 1
+	xvals = []
+	yvals = []
+	for pt in pts:
+		xvals.append(pt[0])
+		yvals.append(pt[1])
+	minX = np.min(xvals)
+	maxX = np.max(xvals)
+	minY = np.min(yvals)
+	maxY = np.max(yvals)
+	rescaleX = lambda x: minX + (maxX-minX) * (x-_minX) / (_maxX-_minX)
+	rescaleY = lambda y: minY + (maxY-minY)* (y-_minY) / (_maxY-_minY)
+	return rescaleX, rescaleY
+
+def packShort(bits):
+	place = 0
+	value = 0
+	for bit in reversed(bits):
+		value+=bit*(2**place)
+		place+=1
+	return value
+
+def addADBits(name,bits):
+	floatData = []
+	for bitNumber in range(0,len(bits),16):
+		bts = bits[bitNumber:(bitNumber+16)]
+		floatData.append(packShort(bts))
+	addArbitraryData(name,floatData)
+
+def testPoint(letter,point):
+	rescaleX,rescaleY = getInverseScalers(letter)
+	testPoint = (rescaleX(point[0]),rescaleY(point[1]))
+	pen=PointInsidePen(glyphSet,testPoint)
+	g = glyphSet[cmap[ord(letter)]]
+	g.draw(pen)
+	return pen.getResult()
+
+class InterceptorPen(TTGlyphPen):
+
+	def getQuadraticSegments(self):
+		return self.quadraticSegments
+
+	def __init__(self,glyphSet,rescaleX,rescaleY):
+	
+		self.rescaleX = rescaleX
+		self.rescaleY = rescaleY
+		self.rescalePoint = lambda p: (rescaleX(p[0]),rescaleY(p[1]))
+		self.quadraticSegments = []
+		self.currentPoint = self.rescalePoint(point(0,0))
+		super().__init__(glyphSet)
+		self.pathStart = self.currentPoint
+
+	def closePath(self):
+		print("Path Closed.")
+		A = self.currentPoint
+		C = self.pathStart
+		B = tuple(midpoint(np.array(A),np.array(C)))
+		self.quadraticSegments.append([A,B,C])
+		super().closePath()
+	def endPath(self):
+		print("Path ended but not closed.")
+		super().endPath()
+	def moveTo(self,pt):
+		print("Moved to point: "+repr(pt))
+		self.currentPoint = self.rescalePoint(pt)
+		self.pathStart = self.currentPoint
+		super().moveTo(pt)
+	def lineTo(self,pt):
+		print("Drew line to point: "+repr(pt))
+		A = self.currentPoint
+		C = self.rescalePoint(pt)
+		B = tuple(midpoint(np.array(A),np.array(C)))
+		print(A,B,C)
+		self.quadraticSegments.append([A,B,C])
+		self.currentPoint = self.rescalePoint(pt)
+		super().lineTo(pt)
+
+	#due to using trueType font, I can assume curveTo will not be called
+	def curveTo(self,*pts):
+		print("Drew cubic curve to: " +repr(pts))
+		super().curveTo(*pts)
+
+	def qCurveTo(self,*pts):
+		if pts[-1] is None:
+			raise Exception("The glyph drawn by this pen contains the rare case for a qCurveTo segment has only an off-curve last point.")
+		print("Drew quadratic curve to: " +repr(pts))
+		L = 1 + len(pts)
+		if L < 3:
+			raise Exception("The glyph has a quadratic segment with only two points.")
+		full_pts = []
+		full_pts.append(self.currentPoint)
+		for I in range(len(pts)-2):
+			A = self.rescalePoint(pts[I])
+			B = self.rescalePoint(pts[I+1])
+			full_pts.append(A)
+			full_pts.append(tuple(midpoint(np.array(A),np.array(B))))
+		full_pts.append(self.rescalePoint(pts[-2]))
+		full_pts.append(self.rescalePoint(pts[-1]))
+		print(L,len(full_pts))
+		L = len(full_pts)
+			
+		start = 0
+		end = 2
+		while end  < L:
+			self.quadraticSegments.append([full_pts[start],full_pts[start+1],full_pts[start+2]])
+			start +=2
+			end +=2
+			
+
+		self.currentPoint = self.rescalePoint(pts[-1])
+		super().qCurveTo(*pts)
+
 LETTER_RESOLUTION = 64
 
 define_auxillary_function("""
@@ -111,235 +289,54 @@ float quadraticBezierSDF(float3 v,float3 A, float3 B, float3 C, float thickness,
 
 )
 
-scene_brush = define_brush(body="""
+def getLetterComponent(letter):
 
-LETTER_AD_OFFS = AD_LETTER_OFFS_F;
+	letter_brush = define_brush(body="""
 
-v=(double3)(2.0*v.x,2.0*v.y,2.0*v.z);
+	LETTER_AD_OFFS = AD_LETTER_OFFS_<{LETTER}>;
 
-int numCurves = (int)getAD(AD_NUMCURVES,0);
-float d = MAX_DISTANCE;
+	v=(double3)(2.0*v.x,2.0*v.y,2.0*v.z);
 
-
-for(int i=0;i<numCurves;i++){
-
-	int offs = i*(9+2);
-	d = T_min(d,quadraticBezierSDF(toVector3f(v),
-		Vector3f(getAD(AD_CURVEDATA,offs+0),getAD(AD_CURVEDATA,offs+1),getAD(AD_CURVEDATA,offs+2)),
-		Vector3f(getAD(AD_CURVEDATA,offs+3),getAD(AD_CURVEDATA,offs+4),getAD(AD_CURVEDATA,offs+5)),
-		Vector3f(getAD(AD_CURVEDATA,offs+6),getAD(AD_CURVEDATA,offs+7),getAD(AD_CURVEDATA,offs+8)),
-		getAD(AD_CURVEDATA,offs+9),(int)getAD(AD_CURVEDATA,offs+10),SUBSEGMENTS
-	));
+	int numCurves = (int)getAD(AD_NUMCURVES_<{LETTER}>,0);
+	float d = MAX_DISTANCE;
 
 
-}
+	for(int i=0;i<numCurves;i++){
 
-return T_max(d,box3(toVector3f(v),Vector3f(0.0,0.0,0.0),Vector3f(1.0,1.0,1.0)));
-
-""")
-
-def vec3(x,y,z):
-	return np.array([x,y,z])
-
-# ######
-#Courtesy of everestial007 on StackOverflow
-#https://stackoverflow.com/a/42815781/5166365
-from fontTools.ttLib import TTFont
-font = TTFont('Designs/Roboto-Black.ttf')
-# ######
-
-cmap = font.getBestCmap()
-glyphSet = font.getGlyphSet()
+		int offs = i*(9+2);
+		d = T_min(d,quadraticBezierSDF(toVector3f(v),
+			Vector3f(getAD(AD_CURVEDATA_<{LETTER}>,offs+0),getAD(AD_CURVEDATA_<{LETTER}>,offs+1),getAD(AD_CURVEDATA_<{LETTER}>,offs+2)),
+			Vector3f(getAD(AD_CURVEDATA_<{LETTER}>,offs+3),getAD(AD_CURVEDATA_<{LETTER}>,offs+4),getAD(AD_CURVEDATA_<{LETTER}>,offs+5)),
+			Vector3f(getAD(AD_CURVEDATA_<{LETTER}>,offs+6),getAD(AD_CURVEDATA_<{LETTER}>,offs+7),getAD(AD_CURVEDATA_<{LETTER}>,offs+8)),
+			getAD(AD_CURVEDATA_<{LETTER}>,offs+9),(int)getAD(AD_CURVEDATA_<{LETTER}>,offs+10),SUBSEGMENTS
+		));
 
 
-draw(scene_brush,Transform.initial(
-	position=vec3(0.0,0.0,0.0),
-	yaw=0,
-	pitch=0,
-	roll=0,
-	scale=vec3(1.0,1.0,1.0)
-))
+	}
 
-curves = []
+	return T_max(d,box3(toVector3f(v),Vector3f(0.0,0.0,0.0),Vector3f(1.0,1.0,1.0)));
 
-AXES_XYZ  = -1
-AXES_XY = 0
-AXES_YZ = 1
-AXES_ZX = 2
+	""".replace("<{LETTER}>",letter))
 
-class Curve:
+	curves = []
 
-	def __init__(self,A,B,C,thickness = 0.075,axesTag=AXES_XY):
-		self.A = A
-		self.B = B
-		self.C = C
 
-		self.thickness = thickness
-		self.axesTag =axesTag
 		
-def addCurve(curve):
-	curves.append(curve)
-
-def midpoint(A,B):
-	return 0.5*(A+B)
+	def addCurve(curve):
+		curves.append(curve)
 
 
-#render ttf here
-
-def getFontData(chr):
-	pen = TTGlyphPen(glyphSet)
-	g = glyphSet[cmap[ord(chr)]]
-	g.draw(pen)
-	g=pen.glyph()
-	return g.getCoordinates(glyphSet._glyphs)
-
-def getScalers(letter):
-	pts = getFontData(letter)[0]
-	minX = -1
-	maxX = 1
-	minY = -1
-	maxY = 1
-	xvals = []
-	yvals = []
-	for pt in pts:
-		xvals.append(pt[0])
-		yvals.append(pt[1])
-	_minX = np.min(xvals)
-	_maxX = np.max(xvals)
-	_minY = np.min(yvals)
-	_maxY = np.max(yvals)
-	rescaleX = lambda x: minX + (maxX-minX) * (x-_minX) / (_maxX-_minX)
-	rescaleY = lambda y: minY + (maxY-minY)* (y-_minY) / (_maxY-_minY)
-	return rescaleX, rescaleY
-
-def getInverseScalers(letter):
-	pts = getFontData(letter)[0]
-	_minX = -1
-	_maxX = 1
-	_minY = -1
-	_maxY = 1
-	xvals = []
-	yvals = []
-	for pt in pts:
-		xvals.append(pt[0])
-		yvals.append(pt[1])
-	minX = np.min(xvals)
-	maxX = np.max(xvals)
-	minY = np.min(yvals)
-	maxY = np.max(yvals)
-	rescaleX = lambda x: minX + (maxX-minX) * (x-_minX) / (_maxX-_minX)
-	rescaleY = lambda y: minY + (maxY-minY)* (y-_minY) / (_maxY-_minY)
-	return rescaleX, rescaleY
-
-def point(a,b):
-	return (a,b)
-
-class InterceptorPen(TTGlyphPen):
-
-	def getQuadraticSegments(self):
-		return self.quadraticSegments
-
-	def __init__(self,glyphSet,rescaleX,rescaleY):
-	
-		self.rescaleX = rescaleX
-		self.rescaleY = rescaleY
-		self.rescalePoint = lambda p: (rescaleX(p[0]),rescaleY(p[1]))
-		self.quadraticSegments = []
-		self.currentPoint = self.rescalePoint(point(0,0))
-		super().__init__(glyphSet)
-		self.pathStart = self.currentPoint
-
-	def closePath(self):
-		print("Path Closed.")
-		A = self.currentPoint
-		C = self.pathStart
-		B = tuple(midpoint(np.array(A),np.array(C)))
-		self.quadraticSegments.append([A,B,C])
-		super().closePath()
-	def endPath(self):
-		print("Path ended but not closed.")
-		super().endPath()
-	def moveTo(self,pt):
-		print("Moved to point: "+repr(pt))
-		self.currentPoint = self.rescalePoint(pt)
-		self.pathStart = self.currentPoint
-		super().moveTo(pt)
-	def lineTo(self,pt):
-		print("Drew line to point: "+repr(pt))
-		A = self.currentPoint
-		C = self.rescalePoint(pt)
-		B = tuple(midpoint(np.array(A),np.array(C)))
-		print(A,B,C)
-		self.quadraticSegments.append([A,B,C])
-		self.currentPoint = self.rescalePoint(pt)
-		super().lineTo(pt)
-
-	#due to using trueType font, I can assume curveTo will not be called
-	def curveTo(self,*pts):
-		print("Drew cubic curve to: " +repr(pts))
-		super().curveTo(*pts)
-
-	def qCurveTo(self,*pts):
-		if pts[-1] is None:
-			raise Exception("The glyph drawn by this pen contains the rare case for a qCurveTo segment has only an off-curve last point.")
-		print("Drew quadratic curve to: " +repr(pts))
-		L = 1 + len(pts)
-		if L < 3:
-			raise Exception("The glyph has a quadratic segment with only two points.")
-		full_pts = []
-		full_pts.append(self.currentPoint)
-		for I in range(len(pts)-2):
-			A = self.rescalePoint(pts[I])
-			B = self.rescalePoint(pts[I+1])
-			full_pts.append(A)
-			full_pts.append(tuple(midpoint(np.array(A),np.array(B))))
-		full_pts.append(self.rescalePoint(pts[-2]))
-		full_pts.append(self.rescalePoint(pts[-1]))
-		print(L,len(full_pts))
-		L = len(full_pts)
-			
-		start = 0
-		end = 2
-		while end  < L:
-			self.quadraticSegments.append([full_pts[start],full_pts[start+1],full_pts[start+2]])
-			start +=2
-			end +=2
-			
-
-		self.currentPoint = self.rescalePoint(pts[-1])
-		super().qCurveTo(*pts)
 
 
-letter = "f"
-pen = InterceptorPen(glyphSet,*getScalers(letter))
-g = glyphSet[cmap[ord(letter)]]
-g.draw(pen)
 
-for segment in pen.getQuadraticSegments():
-	addCurve(Curve(vec3(segment[0][0],segment[0][1],0.0),vec3(segment[1][0],segment[1][1],0.0),vec3(segment[2][0],segment[2][1],0.0)))
-
-def packShort(bits):
-	place = 0
-	value = 0
-	for bit in reversed(bits):
-		value+=bit*(2**place)
-		place+=1
-	return value
-
-def addADBits(name,bits):
-	floatData = []
-	for bitNumber in range(0,len(bits),16):
-		bts = bits[bitNumber:(bitNumber+16)]
-		floatData.append(packShort(bts))
-	addArbitraryData(name,floatData)
-
-def testPoint(letter,point,):
-	rescaleX,rescaleY = getInverseScalers(letter)
-	testPoint = (rescaleX(point[0]),rescaleY(point[1]))
-	pen=PointInsidePen(glyphSet,testPoint)
+	pen = InterceptorPen(glyphSet,*getScalers(letter))
 	g = glyphSet[cmap[ord(letter)]]
 	g.draw(pen)
-	return pen.getResult()
+
+	for segment in pen.getQuadraticSegments():
+		addCurve(Curve(vec3(segment[0][0],segment[0][1],0.0),vec3(segment[1][0],segment[1][1],0.0),vec3(segment[2][0],segment[2][1],0.0)))
+
+
 
 letterbits = []
 for row in range (LETTER_RESOLUTION + 1):
