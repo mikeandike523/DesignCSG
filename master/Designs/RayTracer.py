4dbcf22a9b7a9b2b719b6c192c0b3fd3ea797b6e
Michael Sohnen <michaelsohnenacademic@gmail.com>
2022-06-02 10:21:08 -0400

    Read to finish making an attractive scene.
    Former-commit-id: af463269a7c788b0977345fcb830e1fde2ba30c3 [formerly ce4394fcb0bac7bb779b7ae29a4743b402731c37]
    Former-commit-id: 62cd591bdd5f94829cca05d8510409cc1b438e2f

--- master/Designs/RayTracer.py
+++ master/Designs/RayTracer.py
@@ -4,51 +4,17 @@ import itertools
 import struct
 from math import sqrt,cos,sin
 
-trs , aspect= loadTrianglesFromOBJ("Assets/Model/10600_RC_ Car_SG_v2_L3.obj",scale=5.0)
-
 np.random.seed(1999)
 
+trs , aspect= loadTrianglesFromOBJ("Assets/Model/10600_RC_ Car_SG_v2_L3.obj",scale=5.0)
 for tr in trs:
-	tr.Specular = 0.0
-	tr.Color =vec3(1,1,1)
 	addTriangle(tr)
 
-R=1.5
+R=3.0
 trs = getCircleTriangles(vec3(0.0,-aspect[1],0.0),R,vec3(1,0,0),vec3(0,1,0),vec3(0,0,1),64)
 for tr in trs:
-
 	tr.Specular=1.0
-	tr.Color = vec3(0.0,0.0,0.0)
 	addTriangle(tr)
-
-for I in range(3):
-
-	angle = 2.0*np.pi*I/3
-	rx = vec3(cos(angle),0.0,sin(angle))
-	rz = vec3(cos(angle+np.pi/2),0.0,sin(angle+np.pi/2))
-	ry=vec3(0.0,1.0,0.0)
-
-	center = 2.0*aspect
-	ydir = normalize(aspect)
-	r=normalize(vec3(ydir[0],0.0,ydir[2]))
-	d=np.dot(ydir,r)
-	h=ydir[1]
-	dnew = -h
-	hnew = d
-	xdir = dnew*r + hnew*vec3(0.0,1.0,0.0)
-	zdir = normalize(cross(xdir,ydir))
-
-	xdir = toCoordinates(xdir,rx,ry,rz)
-	ydir = toCoordinates(ydir,rx,ry,rz)
-	zdir = toCoordinates(zdir,rx,ry,rz)
-	center=toCoordinates(center,rx,ry,rz)
-
-	trs = getCircleTriangles(center,R,xdir,ydir,zdir,64)
-	for tr in trs:
-		tr.Emmissive = 1.0
-		tr.Color = [vec3(1.0,0.0,0.0),vec3(0.0,1.0,0.0),vec3(0.0,0.0,1.0)][I]
-		addTriangle(tr)
-
 
 setSamples(256);
 setViewportSamples(1);
